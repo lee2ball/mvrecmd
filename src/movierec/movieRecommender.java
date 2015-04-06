38ab41791db32bf0cd9b1c784e5abc2125dcc187
panjf2000 <panjf2000@gmail.com>
2015-04-06 23:40:53 +0800

    浏览推荐电影模块主界面

---
+++ src/movierec/movieRecommender.java
@@ -0,0 +1,29 @@
+﻿package movierec;
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class movieRecommender {
+	public static void main(String[] args) throws ParseException {
+		File lockFile=new File(sys.fileLock);
+		  //System.out.println(now);
+		if(!lockFile.exists()){
+			//db toDB=new db();
+			dataToDB dt=new dataToDB();
+			dt.readDataToDB("u.data","	");
+			dt.readDataToDB("u.genre","|");
+			////dt.readDataToDB("u.info","	");//need to adjust a little
+			dt.readDataToDB("u.item","|");//need to adjust much
+			dt.readDataToDB("u.user","|");
+			try{
+				lockFile.createNewFile();
+			}catch(Exception e){
+				e.printStackTrace();
+			}//catch
+		}else{
+				System.out.println("file.lock has been created. ");
+				System.out.println("If you want to put data to database,please delete file.lock first!");
+				mainFrame mf=new mainFrame();		
+		}//else
+	}
+}
