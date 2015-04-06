d4b74342e80392c36ec8c80c8497a65a5e6a4aa3
panjf2000 <panjf2000@gmail.com>
2015-04-06 23:38:05 +0800

    数值工具类，用于操作各类数值

---
+++ src/movierec/fun.java
@@ -0,0 +1,30 @@
+﻿package movierec;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
+
+public class fun {
+	public static void showTip(String mes,String title){//弹出对话框窗口，进行操作提示
+		JOptionPane.showMessageDialog(null, mes, title,JOptionPane.INFORMATION_MESSAGE);
+	}
+	public static boolean isInt(String str){//判断数量格式是否是正整数
+		Pattern   p  =   null; 
+		Matcher   m   =   null; 
+		String   mod  =   "^[1-9]+[0-9]*$";
+		p   =   Pattern.compile(mod); 
+		m   =   p.matcher(str); 
+		if(m.matches()) 
+			return   true; 
+		else 
+			return   false;
+	}
+	public static String getDateTime(){
+		Date time = new Date();  
+		DateFormat df = DateFormat.getDateTimeInstance();  
+		return df.format(time);  
+	}
+}
