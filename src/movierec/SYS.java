0b565712ef8b3141c7528449b16bff98b98c7d16
panjf2000 <panjf2000@gmail.com>
2015-04-07 12:31:12 +0800

    工程系统配置文件，存储系统常量

---
+++ src/movierec/SYS.java
@@ -0,0 +1,15 @@
+﻿package movierec;
+
+public class SYS {
+	public static final String fileDir = "ml-100k/";
+	public static final String fileLock = "file.lock";
+	public static final String genre[]={
+		"unknown","action","adventure","animation","children",
+		"comedy","crime","documentary","drama","fantasy",
+		"filmnoir","horror","musical","mystery","romance",
+		"scifi","thriller","war","western"
+	};
+	public static final double recThreshold=3.5;
+	public static final int recNum=20;
+	public static final  double wi=8.524;
+}
